c8df06d98ff837f00edda989209f66d806792364
h2okisme_UIT57 <24520557@gm.uit.edu.vn>
2026-01-09 12:57:15 +0700

    Add files via upload

--- 0.On-tap-cuoi-ky-thuc-hanh/Bai-tap-them-1/Bai-6-Binary-Search-1.cpp
+++ 0.On-tap-cuoi-ky-thuc-hanh/Bai-tap-them-1/Bai-6-Binary-Search-1.cpp
@@ -5,6 +5,23 @@
 
 using namespace std;
 
+int BinarySearch(const vector<int>& a, int x, int& count) {
+    int left = 0;
+    int right = a.size() - 1;
+    count = 0;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        count++;
+        if (a[mid] == x) {
+            return mid;   
+        } else if (a[mid] < x) {
+            left = mid + 1;
+        } else {
+            right = mid - 1;
+        }
+    }
+    return -1;
+}
 int main() {
     int n;
     cin >> n;
@@ -14,25 +31,10 @@ int main() {
     }
     int x;
     cin >> x;
-    int left = 0;
-    int right = n - 1;
-    int pos = -1;
-    int count = 0;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        count++;
-        if (a[mid] == x) {
-            pos = mid;
-            break;   
-        } else if (a[mid] < x) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
+    int count;
+    int pos = BinarySearch(a, x, count);
     cout << pos << "\n";
     if (pos != -1) {
         cout << count << "\n";
-        }
-    return 0;
+    }
 }
